e8c7078a2b6af92ee0946cd9259d7cf6c02f83f4
suveybesena <suveybesena@gmail.com>
2022-06-12 01:30:42 +0300

    add hilt application

---
+++ app/src/main/java/com/example/capstoneproject/CapstoneProjectApplication.kt
@@ -0,0 +1,8 @@
+package com.example.capstoneproject
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CapstoneProjectApplication : Application() {
+}
