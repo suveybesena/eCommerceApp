73119ad24281360f236f0bb48113829c3ee41caa
suveybesena <suveybesena@gmail.com>
2022-06-14 11:57:14 +0300

    update user dao

--- app/src/main/java/com/example/capstoneproject/data/local/user/UserDAO.kt
+++ app/src/main/java/com/example/capstoneproject/data/local/user/UserDAO.kt
@@ -1,21 +1,27 @@
 package com.example.capstoneproject.data.local.user
 
 import androidx.room.*
-import com.example.capstoneproject.common.extensions.Constant
-import com.example.capstoneproject.data.model.user.UserItem
+import com.example.capstoneproject.common.Constant
+import com.example.capstoneproject.data.model.user.User
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface UserDAO {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertUser(user: UserItem)
+    suspend fun insertUser(user: User)
 
     @Update
-    suspend fun updateUser(user: UserItem)
+    suspend fun updateUser(user: User)
 
     @Delete
-    suspend fun deleteUser(user: UserItem)
+    suspend fun deleteUser(user: User)
 
     @Query("SELECT * FROM ${Constant.USER_DB_NAME}")
-    fun getAllUsers(): Flow<List<UserItem>>
+    fun getAllUsers(): Flow<List<User>>
+
+    @Query("SELECT * FROM ${Constant.USER_DB_NAME} WHERE id= :userId ")
+    suspend fun getCurrentUser(userId: Int): User
+
+    @Query("SELECT * FROM ${Constant.USER_DB_NAME} WHERE username = :userName AND password =:userPassword")
+    suspend fun login(userName: String, userPassword: String): User
 }
