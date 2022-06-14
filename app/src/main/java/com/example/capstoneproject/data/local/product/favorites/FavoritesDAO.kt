20ad6f25f79859eadf536952ff4ee6ad57f3e8af
suveybesena <suveybesena@gmail.com>
2022-06-14 11:56:22 +0300

    add favorites dao

---
+++ app/src/main/java/com/example/capstoneproject/data/local/product/favorites/FavoritesDAO.kt
@@ -0,0 +1,21 @@
+package com.example.capstoneproject.data.local.product.favorites
+
+import androidx.room.*
+import com.example.capstoneproject.common.Constant
+import com.example.capstoneproject.data.model.product.Favorites
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface FavoritesDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertProduct(favorites: Favorites)
+
+    @Update
+    suspend fun updateProduct(favorites: Favorites)
+
+    @Delete
+    suspend fun deleteProduct(favorites: Favorites)
+
+    @Query("SELECT * FROM ${Constant.FAVORITES_DB_NAME} WHERE currentUserId = :userId")
+    fun getFavoritesProducts(userId: String): Flow<List<Favorites>>
+}
