d21dbf7530526eae6eb33bd201e19fc0a5699ff9
suveybesena <suveybesena@gmail.com>
2022-06-14 11:56:45 +0300

    add purchased dao

---
+++ app/src/main/java/com/example/capstoneproject/data/local/product/purchased/PurchasedDAO.kt
@@ -0,0 +1,22 @@
+package com.example.capstoneproject.data.local.product.purchased
+
+import androidx.room.*
+import com.example.capstoneproject.common.Constant
+import com.example.capstoneproject.data.model.product.Purchased
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface PurchasedDAO {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertProduct(purchased: Purchased)
+
+    @Update
+    suspend fun updateProduct(purchased: Purchased)
+
+    @Delete
+    suspend fun deleteProduct(purchased: Purchased)
+
+    @Query("SELECT * FROM ${Constant.PURCHASED_DB_NAME} WHERE currentUserId = :userId")
+    fun getPurchasedProducts(userId: String): Flow<List<Purchased>>
+}
