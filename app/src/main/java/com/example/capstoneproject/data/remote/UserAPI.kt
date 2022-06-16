e6d89c300eab920144d5171e22e8533ff727d6f2
suveybesena <suveybesena@gmail.com>
2022-06-16 23:36:51 +0300

    add userAPI

--- app/src/main/java/com/example/capstoneproject/data/remote/UserAPI.kt
+++ app/src/main/java/com/example/capstoneproject/data/remote/UserAPI.kt
@@ -1,4 +1,24 @@
 package com.example.capstoneproject.data.remote
 
+import com.example.capstoneproject.common.Constant.ADD_USER_ENDPOINT
+import com.example.capstoneproject.common.Constant.ALL_USERS_ENDPOINT
+import com.example.capstoneproject.common.Constant.AUTH_ENDPOINT
+import com.example.capstoneproject.data.entities.user.UserItem
+import retrofit2.http.*
+
 interface UserAPI {
+
+    @POST(ADD_USER_ENDPOINT)
+    suspend fun signUp(@Body user: UserItem): UserItem
+
+    @FormUrlEncoded
+    @POST(AUTH_ENDPOINT)
+    suspend fun signIn(
+        @Field("username") username: String,
+        @Field("password") password: String
+    ): Map<String, String>
+
+    @Headers("withToken: true")
+    @GET(ALL_USERS_ENDPOINT)
+    suspend fun getUsers(): List<UserItem>
 }
