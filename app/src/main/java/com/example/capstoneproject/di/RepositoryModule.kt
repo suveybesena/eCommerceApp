60ebd7746874a49b3d3c911c53a4808911761000
suveybesena <suveybesena@gmail.com>
2022-06-17 23:55:55 +0300

    add repository module

---
+++ app/src/main/java/com/example/capstoneproject/di/RepositoryModule.kt
@@ -0,0 +1,24 @@
+package com.example.capstoneproject.di
+
+import com.example.capstoneproject.data.repository.LocalRepositoryImpl
+import com.example.capstoneproject.data.repository.RemoteRepositoryImpl
+import com.example.capstoneproject.domain.repository.LocalRepository
+import com.example.capstoneproject.domain.repository.RemoteRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindRemoteRepository(remoteRepository: RemoteRepositoryImpl): RemoteRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindLocalRepository(localRepository: LocalRepositoryImpl): LocalRepository
+}
