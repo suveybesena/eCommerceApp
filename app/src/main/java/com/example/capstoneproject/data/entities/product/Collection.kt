94bbb19cff2db695ea19831377296663e23ad34b
suveybesena <suveybesena@gmail.com>
2022-06-17 23:57:34 +0300

    add model

---
+++ app/src/main/java/com/example/capstoneproject/data/entities/product/Collection.kt
@@ -0,0 +1,21 @@
+package com.example.capstoneproject.data.entities.product
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.capstoneproject.common.Constant
+
+@Entity(tableName = Constant.COLLECTION_DB_NAME)
+data class Collection(
+    @ColumnInfo(name = Constant.LOCAL_DB_PRODUCT_TITLE)
+    var productName: String?,
+    @ColumnInfo(name = Constant.LOCAL_DB_PRODUCT_CURRENT_USER_UD)
+    val currentUserId: String?,
+    @ColumnInfo(name = Constant.LOCAL_DB_PRODUCT_PRICE)
+    val productPrice: String?,
+    @ColumnInfo(name = Constant.LOCAL_DB_PRODUCT_IMAGE)
+    val productImage: String?,
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = Constant.LOCAL_DB_PRODUCT_ID)
+    val productId: Int? = null
+)
