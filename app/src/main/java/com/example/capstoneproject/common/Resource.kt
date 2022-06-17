7cc61a2b8e858f7eb64185d3f203b0d8d70c2eb0
suveybesena <suveybesena@gmail.com>
2022-06-17 23:55:45 +0300

    add resource

---
+++ app/src/main/java/com/example/capstoneproject/common/Resource.kt
@@ -0,0 +1,8 @@
+package com.example.capstoneproject.common
+
+
+sealed class Resource<T> {
+    data class Error<T>(var message: String? = null) : Resource<T>()
+    data class Success<T>(var data: T? = null) : Resource<T>()
+    object Loading : Resource<Nothing>()
+}
