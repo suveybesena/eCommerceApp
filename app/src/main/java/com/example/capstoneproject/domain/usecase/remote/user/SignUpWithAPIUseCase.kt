3e244d2a1ec9bb62c2f48257fce9eab4f729c2de
suveybesena <suveybesena@gmail.com>
2022-06-17 23:55:13 +0300

    add api sign up

--- app/src/main/java/com/example/capstoneproject/domain/usecase/remote/user/SignUpWithAPIUseCase.kt
+++ app/src/main/java/com/example/capstoneproject/domain/usecase/remote/user/SignUpWithAPIUseCase.kt
@@ -1,4 +1,4 @@
-package com.example.capstoneproject.domain.usecase.remote
+package com.example.capstoneproject.domain.usecase.remote.user
 
 import com.example.capstoneproject.common.Resource
 import com.example.capstoneproject.di.IoDispatcher
@@ -9,15 +9,21 @@ import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 
-class GetProductsBySearchUseCase @Inject constructor(
+class SignUpWithAPIUseCase @Inject constructor(
     private val remoteRepository: RemoteRepository,
     @IoDispatcher private val ioDispatcher: CoroutineDispatcher
 ) {
-    suspend fun invoke(description: String) = flow {
+    suspend fun invoke(
+        email: String,
+        password: String,
+        name: String,
+        phone: String,
+        address: String
+    ) = flow {
         emit(Resource.Loading)
         try {
-            val productsBySearch = remoteRepository.getProductBySearch(description)
-            emit(Resource.Success(productsBySearch))
+            val signUp = remoteRepository.signUp(email, password, name, phone, address)
+            emit(Resource.Success(signUp))
         } catch (e: Exception) {
             emit(Resource.Error(e.localizedMessage))
         }
