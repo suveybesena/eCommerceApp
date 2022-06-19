4cbf3e45bff221944cde17a7a903582d63cc4502
suveybesena <suveybesena@gmail.com>
2022-06-19 17:09:22 +0300

    add events

--- app/src/main/java/com/example/capstoneproject/presentation/categories/CategoriesUiEvent.kt
+++ app/src/main/java/com/example/capstoneproject/presentation/categories/CategoriesUiEvent.kt
@@ -1,5 +1,10 @@
 package com.example.capstoneproject.presentation.categories
 
+import com.example.capstoneproject.data.entities.product.Collection
+import com.example.capstoneproject.data.entities.product.Favorites
+
 sealed class CategoriesUiEvent {
     data class GetProductsByCategoriesUseCase(val categoryName: String) : CategoriesUiEvent()
+    data class InsertProductToFavorites(val favorites: Favorites) : CategoriesUiEvent()
+    data class InsertProductToCollections(val collection: Collection) : CategoriesUiEvent()
 }
