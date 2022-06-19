13132ec57b120c27280274a35f3120ccb272870d
suveybesena <suveybesena@gmail.com>
2022-06-19 17:12:01 +0300

    add api query

--- app/src/main/java/com/example/capstoneproject/data/remote/UserAPI.kt
+++ app/src/main/java/com/example/capstoneproject/data/remote/UserAPI.kt
@@ -1,24 +1,28 @@
 package com.example.capstoneproject.data.remote
 
 import com.example.capstoneproject.common.Constant.ADD_USER_ENDPOINT
-import com.example.capstoneproject.common.Constant.ALL_USERS_ENDPOINT
 import com.example.capstoneproject.common.Constant.AUTH_ENDPOINT
-import com.example.capstoneproject.data.entities.user.UserItem
+import com.example.capstoneproject.data.entities.user.User
+import com.example.capstoneproject.data.model.CRUDResponse
 import retrofit2.http.*
 
 interface UserAPI {
 
-    @POST(ADD_USER_ENDPOINT)
-    suspend fun signUp(@Body user: UserItem): UserItem
-
-    @FormUrlEncoded
     @POST(AUTH_ENDPOINT)
-    suspend fun signIn(
-        @Field("username") username: String,
+    @FormUrlEncoded
+    fun signIn(
+        @Field("email") email: String,
         @Field("password") password: String
-    ): Map<String, String>
+    ): User
 
-    @Headers("withToken: true")
-    @GET(ALL_USERS_ENDPOINT)
-    suspend fun getUsers(): List<UserItem>
+    @POST(ADD_USER_ENDPOINT)
+    @FormUrlEncoded
+    fun signUp(
+        @Field("email") email: String,
+        @Field("password") password: String,
+        @Field("name") name: String,
+        @Field("phone") phone: String,
+        @Field("address") address: String
+    ): CRUDResponse
+
 }
