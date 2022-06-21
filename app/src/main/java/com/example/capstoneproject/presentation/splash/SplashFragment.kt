bcf87d1041bbdad306403e4e473a6a8694fd05d3
suveybesena <suveybesena@gmail.com>
2022-06-21 13:20:01 +0300

    add hilt and shared pref

--- app/src/main/java/com/example/capstoneproject/presentation/splash/SplashFragment.kt
+++ app/src/main/java/com/example/capstoneproject/presentation/splash/SplashFragment.kt
@@ -1,5 +1,6 @@
 package com.example.capstoneproject.presentation.splash
 
+import android.content.Context
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -9,10 +10,15 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.capstoneproject.R
+import com.example.capstoneproject.common.Constant.SHARED_PREF_KEY
 import com.example.capstoneproject.databinding.FragmentSplashBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SplashFragment : Fragment() {
+
     private var splashBinding: FragmentSplashBinding? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -23,8 +29,16 @@ class SplashFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val sharedPref = activity?.getSharedPreferences(
+            "getSharedPref", Context.MODE_PRIVATE
+        )
+        val pref = sharedPref?.getString(SHARED_PREF_KEY, null)
         Handler(Looper.myLooper()!!).postDelayed({
-            findNavController().navigate(R.id.loginRegisterFragment)
+            if (pref != null) {
+                findNavController().navigate(R.id.homeFragment)
+            } else {
+                findNavController().navigate(R.id.loginRegisterFragment)
+            }
         }, 6000)
     }
 
